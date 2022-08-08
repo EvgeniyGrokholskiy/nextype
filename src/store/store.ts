df6646952ee67e7c5ae53a50a31c8bb34517c431
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2022-08-08 10:03:18 +0500

    was configurestore, added store provider

---
+++ src/store/store.ts
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit"
+import {myJsonServerApi} from "./myJsonServerApi/myJsonServerApi"
+
+export const store = configureStore({
+    reducer: {
+        [myJsonServerApi.reducerPath]: myJsonServerApi.reducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(myJsonServerApi.middleware)
+})
